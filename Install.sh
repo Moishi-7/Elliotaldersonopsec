f681322f14244445ad8cb787cb6a0f54654effe8
Moishi-7 <ahmedxnabil2011@gmail.com>
2026-08-29 04:25:44 +0300

    Fix case sensitivity and update echo message

--- Install.sh
+++ Install.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-chmod +x elliotaldersonopsec
+#dl opsec stuff
+chmod +x Elliotaldersonopsec
 
-# Checks /usr/local/bin and ~/.local/bin
+#some more OPSEC 
 if [ -w /usr/local/bin ]; then
-    cp elliotaldersonopsec /usr/local/bin/
+    cp Elliotaldersonopsec /usr/local/bin/elliotaldersonopsec
 elif command -v sudo >/dev/null 2>&1; then
-    sudo cp elliotaldersonopsec /usr/local/bin/
+    sudo cp Elliotaldersonopsec /usr/local/bin/elliotaldersonopsec
 else
     mkdir -p "$HOME/.local/bin"
-    cp elliotaldersonopsec "$HOME/.local/bin/"
+    cp Elliotaldersonopsec "$HOME/.local/bin/elliotaldersonopsec"
 fi
 
-echo "[+] welcome to OPSEC brother, Type 'elliotaldersonopsec'"
+echo "[+] Welcome to OPSEC brother, Type 'elliotaldersonopsec'"
